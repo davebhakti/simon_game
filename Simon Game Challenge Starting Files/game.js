1a1bcf65503a110d0cca4ef218c93747a40df49d
davebhakti <bhaktidave2206@gmail.com>
2025-07-08 10:27:12 -0700

    Check Which Button is Pressed

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,5 +1,7 @@
 var buttonColours = ["red", "blue", "green", "yellow"]
 var gamePattern = [];
+var userClickedPattern = [];
+
 var randomChosenColour = buttonColors[randomNumber];
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
@@ -9,3 +11,8 @@ function nextSequence() {
     var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
 }
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+});
