c5fb3b69e876766202029616f477a5b84139c754
davebhakti <bhaktidave2206@gmail.com>
2025-07-08 10:31:19 -0700

    Added Sounds to Button Clicks

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,18 +1,24 @@
 var buttonColours = ["red", "blue", "green", "yellow"]
 var gamePattern = [];
 var userClickedPattern = [];
-
 var randomChosenColour = buttonColors[randomNumber];
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    playSound(randomChosenColour);
 }
 
 $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
 });
+
+function playSound(name) {
+    var audio = new Audio("sound/" + name + ".mp3");
+    audio.play();
+}
