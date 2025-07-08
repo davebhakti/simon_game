2c75806d70cf1347d81d0e15f798ac6a250f139d
davebhakti <bhaktidave2206@gmail.com>
2025-07-08 01:07:08 -0700

    Generating random tiles

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,11 @@
+var buttonColours = ["red", "blue", "green", "yellow"]
+var gamePattern = [];
+var randomChosenColour = buttonColors[randomNumber];
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+}
