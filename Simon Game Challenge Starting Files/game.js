b154a1faf227dd932ef4706a52eb1a43705cf2e7
davebhakti <bhaktidave2206@gmail.com>
2025-07-09 00:42:16 -0700

    Game Over

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -47,15 +47,25 @@ $(document).keypress(function(){
 });
 
 function checkAnswer(currentLevel) {
-    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
-        console.log("success");
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
 
-    }
     if (userClickedPattern.length === gamePattern.length) {
-        setTimeout(function(){
-            nextSequence();
-        }, 1000);
-    } else {
-        console.log("wrong");
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
     }
+
+  } else {
+    console.log("wrong");
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+  }
 }
